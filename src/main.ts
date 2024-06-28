33c77a703a03ee542323127d4b9dda3f63e0e588
victor <visteblanco@gmail.com>
2024-06-27 22:02:14 -0500

    Ajuste app

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 
+// Habilitar el modo de producción
+enableProdMode();
+
+// Registrar el Service Worker si está disponible en el navegador
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/ngsw-worker.js')
+    .then(() => console.log('Service Worker registrado correctamente'))
+    .catch(err => console.error('Error al registrar el Service Worker', err));
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
